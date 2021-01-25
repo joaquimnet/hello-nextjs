0eefa57cbfde419950f103f6ba58c9bcd26853e4
Joaquim Neto <joaquimmy@yahoo.com>
2021-01-25 12:44:08 -0300

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+function About(props) {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores consequatur deleniti distinctio
+        dolore eaque, eligendi et explicabo fuga illo inventore labore non quisquam reiciendis rem repellendus sit
+        temporibus ut.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ pages/cupcakes/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cupcakes = () => {
+  return (
+    <div>
+      <h1>Do you like cupcakes?</h1>
+    </div>
+  );
+};
+
+export default Cupcakes;
